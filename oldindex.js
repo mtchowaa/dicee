fb7077fbd1947d4b7275c1d3fe1de0893df2d02d
mtchowaa <69306761+mtchowaa@users.noreply.github.com>
2020-08-16 15:07:59 +0800

    Add files via upload

---
+++ oldindex.js
@@ -0,0 +1,45 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1 * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
+
+if (randomNumber1 == 1) {
+    document.querySelectorAll("img")[0].setAttribute("src", "images/dice1.png");
+    }
+    else if (randomNumber1 == 2) {
+      document.querySelectorAll("img")[0].setAttribute("src", "images/dice2.png");
+    }
+    else if (randomNumber1 == 3) {
+      document.querySelectorAll("img")[0].setAttribute("src", "images/dice3.png");
+    }
+    else if (randomNumber1 == 4) {
+      document.querySelectorAll("img")[0].setAttribute("src", "images/dice4.png");
+    }
+    else if (randomNumber1 == 5) {
+      document.querySelectorAll("img")[0].setAttribute("src", "images/dice5.png");
+    } else{
+      document.querySelectorAll("img")[0].setAttribute("src", "images/dice6.png");
+    }
+
+// Player2
+
+    var randomNumber2 = Math.random();
+    randomNumber2 = randomNumber2 * 6;
+    randomNumber2 = Math.floor(randomNumber2) + 1;
+
+    if (randomNumber2 == 1) {
+        document.querySelectorAll("img")[1].setAttribute("src", "images/dice1.png");
+        }
+        else if (randomNumber2 == 2) {
+          document.querySelectorAll("img")[1].setAttribute("src", "images/dice2.png");
+        }
+        else if (randomNumber2 == 3) {
+          document.querySelectorAll("img")[1].setAttribute("src", "images/dice3.png");
+        }
+        else if (randomNumber2 == 4) {
+          document.querySelectorAll("img")[1].setAttribute("src", "images/dice4.png");
+        }
+        else if (randomNumber2 == 5) {
+          document.querySelectorAll("img")[1].setAttribute("src", "images/dice5.png");
+        } else{
+          document.querySelectorAll("img")[1].setAttribute("src", "images/dice6.png");
+        }
